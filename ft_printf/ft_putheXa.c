c45196008e6399244042616bf86cef9d0d4e65a0
said854 <118211256+said854@users.noreply.github.com>
2025-01-31 11:35:46 +0100

    Update ft_putheXa.c

--- ft_printf/ft_putheXa.c
+++ ft_printf/ft_putheXa.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putheXa.c                                       :+:      :+:    :+:   */
+/*   ft_puthexa.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sjoukni <sjoukni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/25 15:43:24 by sjoukni           #+#    #+#             */
-/*   Updated: 2024/11/26 14:56:19 by sjoukni          ###   ########.fr       */
+/*   Created: 2024/11/25 15:26:58 by sjoukni           #+#    #+#             */
+/*   Updated: 2024/11/26 14:56:11 by sjoukni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_puthexa_upper(unsigned int nbr)
+int	ft_puthexa(unsigned int nbr)
 {
 	unsigned int	nb;
 	char			*base;
 	int				count;
 
-	base = "0123456789ABCDEF";
+	base = "0123456789abcdef";
 	nb = (unsigned int)nbr;
 	count = 0;
 	if (nb / 16 > 0)
-		count += ft_puthexa_upper(nb / 16);
+		count += ft_puthexa(nb / 16);
 	count += ft_putchar(base[nb % 16]);
 	return (count);
 }
